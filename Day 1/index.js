a67dec63d53913dd4853d2a4129f5f2cd771ddfb
Ridwan <aladebaruwa@gmail.com>
2023-12-22 13:46:46 +0100

    add

--- Day 1/index.js
+++ Day 1/index.js
@@ -91,10 +91,13 @@ function multiply(arr, n) {
     return product;
   }
 */
-
 function sum(arr, n) {
   // Only change code below this line
-return sum(arr, n - 1) * arr[n-1]
+  if (n <= 0) {
+    return 0;
+  }
+else {
+  return sum(arr, n - 1) * arr[n-1];
+}
   // Only change code above this line
 }
-console.log(sum([1], 0));
