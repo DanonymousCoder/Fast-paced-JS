c93d94fd594c2ee4f1ee3e93308e77e240e78103
Ridwan <aladebaruwa@gmail.com>
2023-12-22 20:28:18 +0100

    add

--- Day 1/index.js
+++ Day 1/index.js
@@ -102,3 +102,55 @@ else {
   // Only change code above this line
 }
 console.log(sum([2,3,4], 1));
+
+
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+for (let i = 0; contacts.length > i; i++) {
+  let contact = contacts[i];
+  console.log(i)
+  if (contact.firstName == name) {
+    if (contact.hasOwnProperty(prop)) {
+      console.log(contact[prop])
+      return contact[prop];
+    }
+    else {
+      return "No such property";
+    }
+  }
+  else {
+    return "No such contact";
+  }
+}
+
+  // Only change code above this line
+}
+
+lookUpProfile("Akira", "likes");
