a5ca19b302349689dfe825f9b184d08474e5aba5
Ridwan <aladebaruwa@gmail.com>
2023-12-22 13:36:21 +0100

    add

--- Day 1/index.js
+++ Day 1/index.js
@@ -72,3 +72,22 @@ function multiplyAll(arr) {
 }
 
 console.log(multiplyAll([[1], [2], [3]]))
+
+
+function multiply(arr, n) {
+  if (n <= 0) {
+    return 1;
+  } else {
+    return multiply(arr, n - 1) * arr[n - 1];
+  }
+}
+
+/* 
+function multiply(arr, n) {
+    let product = 1;
+    for (let i = 0; i < n; i++) {
+      product *= arr[i];
+    }
+    return product;
+  }
+*/
