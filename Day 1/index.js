99b2830bfc5ed3b240b24b764e4a64d72bf5c58f
Ridwan <aladebaruwa@gmail.com>
2023-12-23 16:21:15 +0100

    add

--- Day 1/index.js
+++ Day 1/index.js
@@ -196,3 +196,17 @@ function checkSign(num) {
 
 }
 checkSign(10);
+
+// recursion between range of numbers 
+function rangeOfNumbers(startNum, endNum) {
+  if (startNum > endNum ) {
+    return []
+  }
+  else {
+    const countArray = rangeOfNumbers(startNum + 1, endNum);
+    countArray.unshift(startNum);
+    return countArray;
+  }
+    
+  };
+  console.log(rangeOfNumbers(1, 5))
